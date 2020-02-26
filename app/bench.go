b1cc449dba337d0b0dde174e13335db892e5e3f6
Dainius Matusevicius <dainiauskas@gmail.com>
2020-02-26 12:40:33 +0200

    Added Test records show

--- app/bench.go
+++ app/bench.go
@@ -1,92 +1,95 @@
 package app
 
 import (
-  "fmt"
+	"fmt"
 
-  "bdbu/models"
+	"bdbu/models"
 )
 
 const (
-  tplCreate = "Creating table: %v\n"
-  tplInsert = "Inserting     : %v\n"
-  tplSelect = "Selecting     : %v\n"
-  tplDelete = "Deleting      : %v\n"
-  tplDrop   = "Droping       : %v\n"
-  tplSizeMb = "Table size MB : %v\n"
+	tplRecords = "Test records  : %d\n"
+	tplCreate  = "Creating table: %v\n"
+	tplInsert  = "Inserting     : %v\n"
+	tplSelect  = "Selecting     : %v\n"
+	tplDelete  = "Deleting      : %v\n"
+	tplDrop    = "Droping       : %v\n"
+	tplSizeMb  = "Table size MB : %v\n"
 
-  sqlGetInfo = `engine, row_format,
+	sqlGetInfo = `engine, row_format,
     round(((data_length + index_length) / 1024 / 1024), 2) As size`
 )
 
 type InfoSchema struct {
-  Engine      string  `gorm:"column:ENGINE"`
-  RowFormat   string  `gorm:"column:ROW_FORMAT"`
-  DataLength  float64 `gorm:"column:DATA_LENGTH"`
-  IndexLength float64 `gorm:"column:INDEX_LENGTH"`
+	Engine      string  `gorm:"column:ENGINE"`
+	RowFormat   string  `gorm:"column:ROW_FORMAT"`
+	DataLength  float64 `gorm:"column:DATA_LENGTH"`
+	IndexLength float64 `gorm:"column:INDEX_LENGTH"`
 }
 
 func (InfoSchema) TableName() string {
-  return "information_schema.TABLES"
+	return "information_schema.TABLES"
 }
 
 type TableBench struct {
-  Id    uint      `gorm:"primary_key"`
-  // Str   string    `gorm:"type:char(10);default:''"`
+	Id uint `gorm:"primary_key"`
+	// Str   string    `gorm:"type:char(10);default:''"`
 }
 
 func (TableBench) TableName() string {
-  return "temp_bench_table"
+	return "temp_bench_table"
 }
 
 func Benchmark(records int, engine, rowFormat string) {
-  if (!Config.IsBenchmark()) {
-    return
-  }
+	if !Config.IsBenchmark() {
+		return
+	}
 
-  drv := models.Open(Config.Benchmark, "src")
-  defer drv.Close()
+	drv := models.Open(Config.Benchmark, "src")
+	defer drv.Close()
 
-  db := drv.GetDB()
+	db := drv.GetDB()
 
-  d := NewDuration()
-  err := db.DropTableIfExists(&TableBench{}).
-    Set("gorm:table_options",
-      fmt.Sprintf("ENGINE=%s ROW_FORMAT=%s", engine, rowFormat)).
-    AutoMigrate(&TableBench{}).Error
+	fmt.Printf(tplRecords, records)
 
-  if err != nil {
-    panic(err)
-  }
-  d.Completed(tplCreate)
+	d := NewDuration()
+	err := db.DropTableIfExists(&TableBench{}).
+		Set("gorm:table_options",
+			fmt.Sprintf("ENGINE=%s ROW_FORMAT=%s", engine, rowFormat)).
+		AutoMigrate(&TableBench{}).Error
 
-  d = NewDuration()
-  tx := db.Begin()
-  for i := 0; i < records; i++ {
-    tx.Create(&TableBench{})
-  }
-  tx.Commit()
-  d.Completed(tplInsert)
+	if err != nil {
+		panic(err)
+	}
+	d.Completed(tplCreate)
 
-  list := []TableBench{}
+	d = NewDuration()
+	tx := db.Begin()
+	for i := 0; i < records; i++ {
+		tx.Create(&TableBench{})
+	}
+	tx.Commit()
+	d.Completed(tplInsert)
 
-  d = NewDuration()
-  db.Find(&list)
-  d.Completed(tplSelect)
+	list := []TableBench{}
 
-  d = NewDuration()
-  db.Delete(&list)
-  d.Completed(tplDelete)
+	d = NewDuration()
+	db.Find(&list)
+	d.Completed(tplSelect)
 
-  info := &InfoSchema{}
+	d = NewDuration()
+	db.Delete(&list)
+	d.Completed(tplDelete)
 
-  db.Where("table_schema = ?", Config.Benchmark.Name).
-    Where("table_name = ?", list[0].TableName()).First(&info)
+	info := &InfoSchema{}
 
-  d = NewDuration()
-  db.DropTableIfExists(&TableBench{})
-  d.Completed(tplDrop)
+	db.Where("table_schema = ?", Config.Benchmark.Name).
+		Where("table_name = ?", list[0].TableName()).First(&info)
 
-  fmt.Printf("Table size (MB): %v, Engine: %v, Row format: %v\n",
-    ((info.DataLength + info.IndexLength) / 1024 / 1024),
-    info.Engine, info.RowFormat)
+	d = NewDuration()
+	db.DropTableIfExists(&TableBench{})
+	d.Completed(tplDrop)
+
+	fmt.Printf("Table size (MB): %v, Engine: %v, Row format: %v\n",
+		((info.DataLength + info.IndexLength) / 1024 / 1024),
+		info.Engine, info.RowFormat)
 }
